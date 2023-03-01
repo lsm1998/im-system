ded110958d96dc5e9f8e017fad64f48130c41c22
lsm <487005831@qq.com>
2023-03-01 20:24:54 +0800

    web前端移除

--- web/admin-web/src/api/backend/security/sensitiveData.ts
+++
@@ -1,9 +0,0 @@
-import createAxios from '/@/utils/axios'
-import { securitySensitiveData } from '/@/api/controllerUrls'
-
-export function add() {
-    return createAxios({
-        url: securitySensitiveData + 'add',
-        method: 'get',
-    })
-}
--- web/tenant-web/src/lang/backend/en/auth/group.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    GroupName: 'Group Name',
-    'Group name': 'Group Name',
-    jurisdiction: 'Permissions',
-    'Parent group': 'Superior group',
-    'The parent group cannot be the group itself': 'The parent group cannot be the group itself',
-}
