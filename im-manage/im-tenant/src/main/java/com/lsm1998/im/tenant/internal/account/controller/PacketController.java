ca86a90094d3290c611a915719e7240203df10ed
lsm <487005831@qq.com>
2023-03-04 02:28:52 +0800

    tenant readme

--- im-manage/im-tenant/src/main/java/com/lsm1998/im/tenant/internal/account/controller/PacketController.java
+++ im-manage/im-tenant/src/main/java/com/lsm1998/im/tenant/internal/account/controller/PacketController.java
@@ -1,7 +1,10 @@
 package com.lsm1998.im.tenant.internal.account.controller;
 
+import com.lsm1998.im.common.AjaxResponse;
 import com.lsm1998.im.tenant.internal.account.service.PacketService;
+import com.lsm1998.im.tenant.middleware.interceptor.ContextHolder;
 import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +14,13 @@ public class PacketController
 {
     @Resource
     private PacketService packetService;
+
+    @Resource
+    private ContextHolder contextHolder;
+
+    @GetMapping("/packet")
+    public AjaxResponse getPacket()
+    {
+        return AjaxResponse.success(packetService.getPacket(contextHolder.getAppid()));
+    }
 }
